c44520396af7c51999ae8a41c75cc197cc91e7c2
RahimovIlhom <ilhomjonrahimov217@gmail.com>
2024-12-10 16:04:51 +0500

    webhook bot change

--- web_bot.py
+++ web_bot.py
@@ -39,24 +39,6 @@ async def on_startup() -> None:
     await on_startup_notify()
 
 
-# def main() -> None:
-#     dp.startup.register(on_startup)
-#     dp.shutdown.register(stop_bot)
-#
-#     app = web.Application()
-#
-#     webhook_requests_handler = SimpleRequestHandler(
-#         dispatcher=dp,
-#         bot=bot,
-#         secret_token=WEBHOOK_SECRET,
-#     )
-#     webhook_requests_handler.register(app, path=WEBHOOK_PATH)
-#
-#     setup_application(app, dp, bot=bot)
-#
-#     web.run_app(app, host=WEB_SERVER_HOST, port=WEB_SERVER_PORT)
-
-
 def main() -> None:
     dp.startup.register(on_startup)
     dp.shutdown.register(stop_bot)
@@ -72,11 +54,29 @@ def main() -> None:
 
     setup_application(app, dp, bot=bot)
 
-    context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
-    context.load_cert_chain(WEBHOOK_SSL_CERT, WEBHOOK_SSL_PRIV)
+    web.run_app(app, host=WEB_SERVER_HOST, port=WEB_SERVER_PORT)
 
-    # And finally start webserver
-    web.run_app(app, host=WEB_SERVER_HOST, port=WEB_SERVER_PORT, ssl_context=context)
+
+# def main() -> None:
+#     dp.startup.register(on_startup)
+#     dp.shutdown.register(stop_bot)
+#
+#     app = web.Application()
+#
+#     webhook_requests_handler = SimpleRequestHandler(
+#         dispatcher=dp,
+#         bot=bot,
+#         secret_token=WEBHOOK_SECRET,
+#     )
+#     webhook_requests_handler.register(app, path=WEBHOOK_PATH)
+#
+#     setup_application(app, dp, bot=bot)
+#
+#     context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
+#     context.load_cert_chain(WEBHOOK_SSL_CERT, WEBHOOK_SSL_PRIV)
+#
+#     # And finally start webserver
+#     web.run_app(app, host=WEB_SERVER_HOST, port=WEB_SERVER_PORT, ssl_context=context)
 
 
 if __name__ == "__main__":
